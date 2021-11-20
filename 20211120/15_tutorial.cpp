ab8e26916e0693abc07aaa6b27a33c9e367143fd
tomoyamizukuro <salvatoleyohane@gmail.com>
2021-11-20 18:07:38 +0900

    15tutorial completed

---
+++ 20211120/15_tutorial.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hello(string);
+int main() {
+    hello("Tom");
+    hello("c++");
+}
+
+void hello(string text) {
+    cout << "Hello, " << text << endl;
+    return;
+}
